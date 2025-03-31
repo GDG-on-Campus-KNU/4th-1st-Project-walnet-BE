c17c37c1bb1dd00c53e6b42a5b6d7cdada33f612
LeeDongjae <leedongjae625@gmail.com>
2025-03-31 21:12:55 +0900

    chore: 짜바리 커밋

--- src/main/java/com/walnet/backend/domain/member/controller/MemberController.java
+++ src/main/java/com/walnet/backend/domain/member/controller/MemberController.java
@@ -1,8 +1,10 @@
 package com.walnet.backend.domain.member.controller;
 
 import com.walnet.backend.domain.member.service.MemberService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,5 +15,9 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @GetMapping
+    public void memberInfo(Authentication authentication) {
+        authentication.getName();
+    }
 
 }
